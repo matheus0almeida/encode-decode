560461f67b1af30b6218e3c813a7ef21a9bf5e99
matheus0almeida <77692412+matheus0almeida@users.noreply.github.com>
2022-04-19 19:56:48 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import cryptocode
 import streamlit as st
+from datetime import datetime,date
+import time
+import hashlib
 
 def encoded (key,string):
     str_encoded = cryptocode.encrypt(string, key)
@@ -9,6 +12,11 @@ def decoded (key,string):
     str_decoded = cryptocode.decrypt(string, key)
     return (str_decoded)
 
+def hash ():
+    t_today = datetime.now()
+    s_today = t_today.strftime('%Y/%m/%d %H:%M*%S')
+    crypto = hashlib.sha256(s_today.encode('utf-8')).hexdigest()
+    return (crypto)
 
 st.set_page_config(
      page_title="Criptografar e Descriptografar",
@@ -18,15 +26,32 @@ st.set_page_config(
 
 st.title('Criptografar e Descriptografar')
 
-option = st.radio("Selecione a opção: ",('Criptografar', 'Descriptografar'))
+with st.expander("Cripitografia Simples - Menos Seguro"):
 
-key = st.text_input('Senha', '')
-string = st.text_input('Texto', '')
+    option = st.radio("Selecione a opção: ",('Criptografar', 'Descriptografar'))
+    key = st.text_input('Senha', '')
+    string = st.text_input('Texto', '')
 
-if st.button('Rodar'):
-    if option == 'Criptografar' :
-        st.text(option)
-        st.text(encoded (key,string))
-    if option == 'Descriptografar' :
-        st.text(option)
-        st.text(decoded (key,string))
+    if st.button('Rodar'):
+        if option == 'Criptografar' :
+            st.text("Texto Criptografado")
+            st.text_area("",encoded (key,string))
+        if option == 'Descriptografar' :
+            st.text("Texto Descriptografado")
+            st.text_area("",decoded (key,string))
+
+with st.expander("Cripitografia Complexa - Mais Seguro"):
+
+    if st.button('Criptografar'):
+        key1 = st.text_input('Senha ', hash ())
+        string1 = st.text_input('Texto ', '')
+        if st.button('Rodar '):
+            st.text("Texto Criptografado")
+            st.text_area("",encoded (key1,string1))
+
+    if st.button('Descriptografar'):
+        key1 = st.text_input('Senha ', '')
+        string1 = st.text_input('Texto ', '')
+        if st.button('Rodar  '):
+            st.text("Texto Descriptografado")
+            st.text_area("",decoded (key1,string1))
